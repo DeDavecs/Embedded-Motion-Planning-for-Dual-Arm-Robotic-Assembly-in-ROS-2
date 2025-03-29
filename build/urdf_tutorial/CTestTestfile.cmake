1998bd9a26549731dbccab55ebf22224cf7b2872
David Streib <dstreib@mavt-ide-n106l.d.ethz.ch>
2025-03-29 10:58:41 +0100

    Move contents of workspace directory to main

---
+++ build/urdf_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dstreib/UR5e_Assembly_of_Wooden_Pieces/src/attachments/tutorial_shapes/urdf_tutorial
+# Build directory: /home/dstreib/UR5e_Assembly_of_Wooden_Pieces/build/urdf_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
